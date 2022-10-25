b1197b4d2ae7120d2fe34a137a85cb18a6330a41
YangZer0 <yangzer0@qq.com>
2022-10-25 19:43:43 +0800

    ddpg_0.1

--- walking_droid/learning/actor_critic_model.py
+++ walking_droid/learning/actor_critic_model.py
@@ -1,10 +1,60 @@
+import numpy as np
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class DeepModel(nn.Module):
 
-    def __init__(self):
+def fanin_init(size, fanin=None):
+    fanin = fanin or size[0]
+    v = 1. / np.sqrt(fanin)
+    return torch.Tensor(size).uniform_(-v, v)
+
+
+class ActorNet(nn.Module):
+    def __init__(self, num_inputs, num_actions, num_hidden1=400, num_hidden2=300, init_w=3e-3):
+        super(ActorNet, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, num_hidden1)
+        self.fc2 = nn.Linear(num_hidden1, num_hidden2)
+        self.fc3 = nn.Linear(num_hidden2, num_actions)
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+        self.init_weights(init_w)
+
+    def init_weights(self, init_w):
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3.weight.data.uniform_(-init_w, init_w)
 
     def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        out = self.tanh(out)
+        return out
 
-    def learn(self):
+
+class CriticNet(nn.Module):
+    def __init__(self, num_inputs, num_actions, num_hidden1=400, num_hidden2=300, init_w=3e-3):
+        super(CriticNet, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, num_hidden1)
+        self.fc2 = nn.Linear(num_hidden1 + num_actions, num_hidden2)
+        self.fc3 = nn.Linear(num_hidden2, 1)
+        self.relu = nn.ReLU()
+        self.init_weights(init_w)
+
+    def init_weights(self, init_w):
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3.weight.data.uniform_(-init_w, init_w)
+
+    def forward(self, x, a):
+        out = self.fc1(x)
+        out = self.relu(out)
+        # debug()
+        out = self.fc2(torch.cat([out, a], 1))
+        out = self.relu(out)
+        out = self.fc3(out)
+        return out
--- walking_droid/learning/ddpg_alg.py
+++ walking_droid/learning/ddpg_alg.py
@@ -5,7 +5,9 @@ import torch.nn as nn
 from torch.optim import Adam
 
 from actor_critic_model import ActorNet, CriticNet
-
+from memory import SequentialMemory
+from random_process import OrnsteinUhlenbeckProcess
+from utils.util import *
 
 criterion = nn.MSELoss
 
@@ -26,20 +28,45 @@ class DDPG:
         }
 
         self.actor = ActorNet(self.num_inputs, self.num_actions, **net_cfg)
-        self.actor_target =ActorNet(self.num_inputs, self.num_actions, **net_cfg)
+        self.actor_target = ActorNet(self.num_inputs, self.num_actions, **net_cfg)
         self.actor_optim = Adam(self.actor.parameters(), lr=args.prate)
 
         self.critic = CriticNet(self.num_inputs, self.num_actions, **net_cfg)
         self.critic_target = CriticNet(self.num_inputs, self.num_actions, **net_cfg)
         self.critic_optim = Adam(self.critic.parameters(), lr=args.rate)
 
-    def predict(self, state):
+        self.memory = SequentialMemory(limit=args.rmsize, window_length=args.window_length)
+        self.random_process = OrnsteinUhlenbeckProcess(size=num_actions, theta=args.ou_theta,
+                                                       mu=args.ou_mu, sigma=args.ou_sigma)
 
-    def learn(self, state, action, reward, next_state, terminal):
+        self.batch_size = args.bsize
+        self.tau = args.tau
+        self.discount = args.discount
+        self.depsilon = 1.0 / args.epsilon
 
-    def _critic_learn(self, state, action, reward, next_state, terminal):
+        self.epsilon = 1.0
+        self.s_t = None
+        self.a_t = None
+        self.is_training = True
 
-    def _actor_learn(self, state):
+        if USE_CUDA: self.cude()
 
-    def sync_target(self, decay=None):
+    def update_policy(self):
+        # Sample batch
+        state_batch, action_batch, reward_batch, \
+            next_state_batch, terminal_batch = self.memory.sample_and_split(self.batch_size)
+
+        # Prepare for the target q batch
+        next_q_values = self.critic_target(
+            to_tensor(next_state_batch, volatile=True),
+            self.actor_target(
+                to_tensor(next_state_batch, volatile=True)
+            )
+        )
+        next_q_values.volatile = False
+
+        target_q_batch = to_tensor(reward_batch) + \
+            self.discount * to_tensor(terminal_batch.astype(np.float)) * next_q_values
+
+
 
