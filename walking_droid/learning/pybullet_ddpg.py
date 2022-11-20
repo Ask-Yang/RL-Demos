70f22eaa28e2a1f91dba6560468111e4c9a8c735
YangZer0 <yangzer0@qq.com>
2022-11-20 17:56:02 +0800

    update_ignore

--- walking_droid/learning/pybullet_ddpg.py
+++ walking_droid/learning/pybullet_ddpg.py
@@ -8,6 +8,7 @@ from actor_critic_model import *
 from collector import *
 from ddpg import *
 from trainer import *
+from env.sim_env import *
 
 import gym
 import numpy as np
@@ -41,7 +42,7 @@ def parse_args():
         help="whether to capture videos of the agent performances (check out `videos` folder)")
 
     # Algorithm specific arguments
-    parser.add_argument("--env-id", type=str, default="HopperBulletEnv-v0",
+    parser.add_argument("--env-id", type=str, default="MountainCarContinuous-v0",
         help="the id of the environment")
     parser.add_argument("--total-timesteps", type=int, default=1000000,
         help="total timesteps of the experiments")
@@ -70,11 +71,13 @@ def parse_args():
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
-        env = gym.make(env_id)
-        env = gym.wrappers.RecordEpisodeStatistics(env)
+        # env = gym.make(env_id)
+        env = wdSim()
+        #env = gym.make("MountainCarContinuous-v0")
+        env = gym.wrappers.RecordEpisodeStatistics(env)  # 递归调用所有wrapper的step
         if capture_video:
             if idx == 0:
-                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}", step_trigger=lambda t: t % 10 == 0)
         env.seed(seed)
         env.action_space.seed(seed)
         env.observation_space.seed(seed)
@@ -83,8 +86,9 @@ def make_env(env_id, seed, idx, capture_video, run_name):
     return thunk
 
 
-def mujoco_ddpg(args=parse_args()):
+def mujoco_ddpg():
     args = parse_args()
+    args.env_id = "droid"
     run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
     if args.track:
         import wandb
@@ -110,6 +114,7 @@ def mujoco_ddpg(args=parse_args()):
     torch.manual_seed(args.seed)
     torch.backends.cudnn.deterministic = args.torch_deterministic
 
+    p=torch.cuda.is_available()
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
 
     # env setup
@@ -126,8 +131,8 @@ def mujoco_ddpg(args=parse_args()):
     critic_target = CriticNet(envs).to(device)
     critic_target.load_state_dict(critic.state_dict())
     critic_optim = optim.Adam(list(critic.parameters()), lr=args.learning_rate)
-    policy = DDPGPolicy(actor, actor_target, actor_optim,
-                        critic, critic_target, critic_optim)
+    # policy = DDPGPolicy(actor, actor_target, actor_optim,
+    #                     critic, critic_target, critic_optim)
 
     envs.single_observation_space.dtype = np.float32
     # replay buffer setup
@@ -138,12 +143,87 @@ def mujoco_ddpg(args=parse_args()):
         device,
         handle_timeout_termination=True
     )
-    start_time = time.time()
 
     # setup game and start
-    collector = Collector(envs, args, policy, writer, replay_buffer, device)
-    collector.collect()  # fill replay_buffer
+    obs = envs.reset()
+    for global_step in range(args.total_timesteps):
+        # ALGO LOGIC: put action logic here
+        if global_step < args.learning_starts:
+            actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
+        else:
+            with torch.no_grad():
+                actions = actor(torch.Tensor(obs).to(device))
+                actions += torch.normal(0, actor.action_scale * args.exploration_noise)
+                actions = actions.cpu().numpy().clip(envs.single_action_space.low, envs.single_action_space.high)
 
-    # traing
-    tariner = Trainer()
-    tariner.run()
+        # TRY NOT TO MODIFY: execute the game and log data.
+        next_obs, rewards, dones, infos = envs.step(actions)
+
+        start_time = time.time()
+        # TRY NOT TO MODIFY: record rewards for plotting purposes
+        for info in infos:
+            if "episode" in info.keys():
+                print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
+                writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
+                writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
+                break
+
+        # TRY NOT TO MODIFY: save data to reply buffer; handle `terminal_observation`
+        real_next_obs = next_obs.copy()
+        for idx, d in enumerate(dones):
+            if d and "terminal_observation" in infos[idx]:
+                real_next_obs[idx] = infos[idx]["terminal_observation"]
+        replay_buffer.add(obs, real_next_obs, actions, rewards, dones, infos)
+
+        # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
+        obs = next_obs
+
+        # ALGO LOGIC: training.
+        if global_step > args.learning_starts:
+            data = replay_buffer.sample(args.batch_size)
+            with torch.no_grad():
+                next_state_actions = actor_target(data.next_observations)
+                qf1_next_target = critic_target(data.next_observations, next_state_actions)
+                next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (qf1_next_target).view(-1)
+
+            qf1_a_values = critic(data.observations, data.actions).view(-1)
+            critic_loss = F.mse_loss(qf1_a_values, next_q_value)
+
+            # optimize the model
+            critic_optim.zero_grad()
+            critic_loss.backward()
+            critic_optim.step()
+
+            if global_step % args.policy_frequency == 0:
+                actor_loss = -critic(data.observations, actor(data.observations)).mean()
+                actor_optim.zero_grad()
+                actor_loss.backward()
+                actor_optim.step()
+
+                # update the target network
+                for param, target_param in zip(actor.parameters(), actor_target.parameters()):
+                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+                for param, target_param in zip(critic.parameters(), critic_target.parameters()):
+                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+
+            if global_step % 100 == 0:
+                writer.add_scalar("losses/critic_loss", critic_loss.item(), global_step)
+                writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
+                writer.add_scalar("losses/qf1_values", qf1_a_values.mean().item(), global_step)
+                print("SPS:", int(global_step / (time.time() - start_time)))
+                writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+
+
+    # collector = Collector(envs, args, policy, writer, replay_buffer, device)
+    # collector.collect()  # fill replay_buffer
+    #
+    # # traing
+    # tariner = Trainer(policy, envs, device)
+    # tariner.run(replay_buffer, args, writer)
+
+    envs.close()
+    writer.close()
+
+
+if __name__ == "__main__":
+    mujoco_ddpg()
